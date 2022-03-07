2424826751fe1ac11389286812f385043538977a
João Lucas Pereira <83319546+joaolucasp@users.noreply.github.com>
2022-03-07 15:18:14 -0300

    Adicionado script.js

---
+++ script/script.js
@@ -0,0 +1,35 @@
+function atualizaRelogio(){ 
+    var momentoAtual = new Date();
+    
+    var vhora = momentoAtual.getHours();
+    var vminuto = momentoAtual.getMinutes();
+    var vsegundo = momentoAtual.getSeconds();
+    
+    var vdia = momentoAtual.getDate();
+    var vmes = momentoAtual.getMonth() + 1;
+    var vano = momentoAtual.getFullYear();
+    
+    if (vdia < 10){ vdia = "0" + vdia;}
+    if (vmes < 10){ vmes = "0" + vmes;}
+    if (vhora < 10){ vhora = "0" + vhora;}
+    if (vminuto < 10){ vminuto = "0" + vminuto;}
+    if (vsegundo < 10){ vsegundo = "0" + vsegundo;}
+
+    dataFormat = vdia + "/" + vmes + "/" + vano;
+    horaFormat = vhora + ":" + vminuto + ":" + vsegundo;
+
+    document.getElementById("data").innerHTML = dataFormat;
+    document.getElementById("hora").innerHTML = horaFormat;
+
+    setTimeout("atualizaRelogio()",1000);
+}
+function vefic(){
+    var email = document.getElementById('email')
+
+    if (email.value.length == 0){
+        alert('Por favor, preencha o campo email') 
+    } else{
+        alert('Agradecemos pela participação no site Notícias Cidade! Em breve enviaremos notificações de nosso site!')
+        email.value = ''
+    }
+}
